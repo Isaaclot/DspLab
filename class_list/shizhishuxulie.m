ada17dfb6f3699db8fc126b0c3b13e87b4e2c1c1
liangqian <liangqian0723@gmail.com>
2015-11-08 11:03:02 +0800

    classic

---
+++ class_list/shizhishuxulie.m
@@ -0,0 +1,8 @@
+
+clear;
+
+a=0.5;
+n=0:5;
+x=a.^n;
+subplot(2,2,1);
+stem(n,x);
---
+++ class_list/sinlge_simpling.m
@@ -0,0 +1,13 @@
+clear;
+n=-5:5;
+x=[zeros(1,5) 1 zeros(1,5)]
+
+subplot(2,2,1);
+stem(n,x);
+title('单位取样序列');
+xlabel('n');
+ylabel('X(n)');
+
+
+
+
